d65348ff9c494ebc23e68903c3fcb6754fa49a36
Anton van Wezenbeek <a.m.vanwezenbeek@gmail.com>
2020-12-19 17:17:00 +0100

    SetFocus to stc only in visual mode

--- frame.cpp
+++ frame.cpp
@@ -145,7 +145,8 @@ frame::frame(app* app)
 
   statustext(wex::lexers::get()->theme(), "PaneTheme");
 
-  if (m_editors->GetPageCount() > 0)
+  if (m_editors->GetPageCount() > 0 && 
+    !m_app->data().flags().test(wex::data::stc::WIN_EX))
   {
     m_editors->GetPage(m_editors->GetPageCount() - 1)->SetFocus();
   }
@@ -991,7 +992,10 @@ frame::open_file(const wex::path& filename, const wex::data::stc& data)
       wex::data::stc(editor, data).inject();
     }
 
-    editor->SetFocus();
+    if (!m_app->data().flags().test(wex::data::stc::WIN_EX))
+    {
+      editor->SetFocus();
+    }
 
     return editor;
   }
