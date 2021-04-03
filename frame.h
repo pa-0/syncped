e7bac719e6a72747cdcfad915dc22a197aff09f5
Anton van Wezenbeek <a.m.vanwezenbeek@gmail.com>
2021-04-03 11:57:29 +0200

    temp fix error

--- frame.h
+++ frame.h
@@ -51,7 +51,7 @@ private:
 
   bool output(const std::string& text) const override;
 
-  void print_ex(wex::ex* ex, const std::string& text) override;
+  void print_ex(wex::ex* ex, const std::string& text);
 
   void record(const std::string& command) override;
 
